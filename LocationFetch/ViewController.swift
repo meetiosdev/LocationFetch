b8045a9128a13c65536b35655205fa22960b0b9f
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2023-07-27 13:56:28 +0530

    Initial Commit

---
+++ LocationFetch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationFetch
+//
+//  Created by Swarajmeet Singh on 27/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
